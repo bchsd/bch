146236eaac3af911c6536dfcbe99e44698e97837
Parker Nguyen <lt.nguyen06@outlook.com>
2026-06-09 22:03:56 -0700

    [ADD] basic menu and cprimary color

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Close menu after picking option
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navbarCollapse = document.getElementById('navbarNav');
+        
+        // Only trigger the close animation if the mobile menu is currently open
+        if (navbarCollapse.classList.contains('show')) {
+            const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+            bsCollapse.hide();
+        }
+    });
+});
